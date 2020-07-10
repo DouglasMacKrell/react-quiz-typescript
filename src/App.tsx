9202facdf6bd57c784a68e087bf8884ea0821c48
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-07-10 17:41:00 -0400

    typescript styling complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,108 @@
 import React, { useState } from 'react';
+import { fetchQuizQuestions } from './API'
 //Components
 import QuestionCard from './components/QuestionCard';
+//Types
+import { QuestionState, Difficulty } from './API';
+//Styles
+import { GlobalStyle, Wrapper } from './App.styles';
+
+export type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
 
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
   const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
 
   }
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
-
+    if (!gameOver) {
+      //user's answer
+      const answer = e.currentTarget.value;
+      //check answer against correct value
+      const correct = questions[number].correct_answer === answer;
+      //add score if answer is correct
+      if (correct) setScore(prev => prev + 1);
+      //save answer in array of user answers
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      }
+      setUserAnswers(prev => [...prev, answerObject])
+    }
   }
 
   const nextQuestion = () => {
+    //Move on to the next question (if not the last)
+    const nextQuestion = number + 1;
 
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
   }
 
   return (
-    <div className="App">
-      <h1>REACT QUIZ</h1>
-      <button className="start" onClick={ startTrivia }>
-        Start
-      </button>
-      <p className="score">Score:</p>
-      <p>Loading questions ...</p>
-      <QuestionCard 
-        questionNr={number + 1}
-        totalQuestions={TOTAL_QUESTIONS}
-        question={questions[number].question}
-        answers={questions[number].answers}
-        userAnswer={userAnswers ? userAnswers[number] : undefined}
-        callback={checkAnswer}
-      />
-      <button className="next" onClick={ nextQuestion }>
-        Next Question
-      </button>
-    </div>
+    <>
+      <GlobalStyle /> 
+      <Wrapper>
+        <h1>REACT QUIZ</h1>
+        {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+          <button className="start" onClick={startTrivia}>
+            Start
+          </button>
+        ) : null
+        }
+        {!gameOver ? <p className="score">Score: {score}</p> : null}
+        {loading && <p>Loading questions ...</p>}
+        {!loading && !gameOver && (
+          <QuestionCard
+            questionNr={number + 1}
+            totalQuestions={TOTAL_QUESTIONS}
+            question={questions[number].question}
+            answers={questions[number].answers}
+            userAnswer={userAnswers ? userAnswers[number] : undefined}
+            callback={checkAnswer}
+          />
+        )}
+        {!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
+          <button className="next" onClick={nextQuestion}>
+            Next Question
+          </button>
+
+        ) : null}
+      </Wrapper>
+    </>
   );
-}
+} 
 
 export default App;
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
 //Types
+import { AnswerObject } from '../App'
+
+//Styles
+
+import { Wrapper, ButtonWrapper } from './QuestionCard.styles'
+
+
 type Props = {
     question: string;
     answers: string[];
-    callback: any;
-    userAnswer: any;
+    callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    userAnswer: AnswerObject | undefined;
     questionNr: number;
     totalQuestions: number;
 }
@@ -19,22 +26,26 @@ const QuestionCard: React.FC<Props> = ({
     totalQuestions 
 }) => {
     return (
-        <div>
+        <Wrapper>
             <p className="number">
                 Question: {questionNr} / {totalQuestions}
             </p>
             <p dangerouslySetInnerHTML={{ __html: question }} />
             <div>
                 {answers.map(answer => (
-                    <div>
-                        <button disabled={userAnswer} onClick={callback}>
+                    <ButtonWrapper 
+                        key={answer}
+                        correct={userAnswer?.correctAnswer === answer}
+                        userClicked={userAnswer?.answer === answer}
+                    >
+                        <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
                             <span dangerouslySetInnerHTML={{ __html: answer }} />
                         </button>
-                    </div>
+                    </ButtonWrapper>
                 ))}
             </div>
-        </div>
+        </Wrapper>
     )
-}
+} 
 
 export default QuestionCard;
